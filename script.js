b00f8065b5d4bc695824ad2c1d8a639a33d0b743
Sameer Singh <sameersingh1234pok@gmail.com>
2022-10-22 23:24:58 +0530

    Removed Everything

--- script.js
+++ script.js
@@ -5,26 +5,9 @@ const NoPeople = document.querySelector('[data-no-of-person]');
 const TipPerson = document.querySelector('[data-tip-per-person]');
 const TotalPerson = document.querySelector('[data-total-per-person]');
 const ResetBtn = document.querySelector('[data-reset]');
+const Errors = document.querySelector('[data-error-message]')
+const NoPeopleDiv = document.querySelector('.people-inputs')
 
-const Reset = () => {
-    AmountInput.value = '';
-    CustomTip.value = '';
-    NoPeople.value = '';
-    TipPerson.innerHTML = '$0.00';
-    TotalPerson.innerHTML = '$0.00';
-}
-
-Tips.forEach((tip) => {
-    tip.addEventListener('click', ()=> {{
-        const Inttip = tip.value;
-        const UserInput = AmountInput.value;
-        console.log(parseInt(Inttip) + parseInt(UserInput));
-    }})
-});
-
-ResetBtn.addEventListener('click', ()=> {
-    Reset();
-})
 
 // For Dark Theme Toggle 
 let darkMode = localStorage.getItem('darkMode');
@@ -45,6 +28,7 @@ const DisableDarkMode = () => {
 if (darkMode == 'enabled') {
     EnableDarkMode();
 }
+
 themeBtn.addEventListener('click', ()=> {
     darkMode = localStorage.getItem('darkMode')
     if(darkMode == "enabled") {
